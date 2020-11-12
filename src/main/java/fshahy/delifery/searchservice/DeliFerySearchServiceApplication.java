128435f776c84386b45adc4e36414ee60aa95e28
Farid Shahy <fshahy@gmail.com>
2020-11-12 18:22:54 +0330

    init maven project.

---
+++ src/main/java/fshahy/delifery/searchservice/DeliFerySearchServiceApplication.java
@@ -0,0 +1,13 @@
+package fshahy.delifery.searchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliFerySearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliFerySearchServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fshahy/delifery/searchservice/DeliFerySearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fshahy.delifery.searchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliFerySearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
